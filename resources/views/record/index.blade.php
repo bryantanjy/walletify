1c3f5863ce5cc43b699f24d2ee25e6f13396eb55
Bryan Tan <bryantan8858@gmail.com>
2023-08-28 04:08:36 +0800

    Update index.blade.php

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -5,10 +5,14 @@
         integrity="sha256-OFRAJNoaD8L3Br5lglV7VyLRf0itmoBzWUoM+Sji4/8=" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+    <script src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script src="{{ asset('js/daterangepicker.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/modal.css') }}">
+    <script src="{{ asset('js/modal.js') }}"></script>
 </head>
-
 <x-app-layout>
     <main>
         <aside class="fixed top-16 left-0 z-40 transition-transform -translate-x-full sm:translate-x-0"
@@ -28,15 +32,18 @@
                             <i class="far fa-plus-square mr-1" style="color: #ffffff;"></i>
                             <span>Add</span>
                         </button>
+                        {{-- <a href="{{ route('record.create') }}">Create Record</a> --}}
+
                     </div>
                 </ul>
-                <div class="flex flex-col items-center mt-4">
+                <div class="flex flex-col items-center mt-10">
                     <div>
                         <h2 style="font-weight: bold">FILTER</h2>
                         <h4 style="font-weight: bold">CATEGORIES</h4>
                         @foreach ($categories as $category)
                             <label class="flex items-center">
-                                <input type="checkbox" class="mr-4" name="category[]" value="{{ $category->category_id }}">
+                                <input type="checkbox" class="mr-4" name="category[]"
+                                    value="{{ $category->category_id }}">
                                 {{ $category->category_name }}
                             </label>
                         @endforeach
@@ -52,28 +59,37 @@
                         </label>
                     </div>
                 </div>
-
             </div>
         </aside>
-        <div class="float-right p-4 sm:ml-64 " style="width: 80%;">
+        <div class="float-right p-4 sm:ml-64" style="width: 80%;">
+            <div class="flex items-center mx-auto" style="width:300px">
+                <i class="fa fa-caret-left ml-2 mt-2 cursor-pointer" id="prevMonth"></i>
+                <div id="reportrange"
+                    class="flex mx-auto mt-2 rounded-md border border-white shadow items-center pull-right relative"
+                    style="width: 215px; height: 30px; font-size: 14px; background: #fff; cursor: pointer; padding: 5px 10px;">
+                    <span></span><i class="fa fa-chevron-down absolute top-0 right-0 mt-2 mr-2"
+                        style="font-size: 12px;"></i>
+                </div>
+                <i class="fa fa-caret-right mr-2 mt-2 cursor-pointer" id="nextMonth"></i>
+            </div>
             @if ($records)
-
-                <table class="mt-1 p-4 ml-14" style="width:800px">
-                    <tr class="flex justify-content-between">
-                        <td><input type="checkbox" name="select_all" id="select_all">Select All</td>
-                        <td>RM9999.00</td>
-                    </tr>
+                <div class="mt-8 px-2 ml-14 " style="width:90%">
+                    <div class="grid grid-cols-2 px-5 bg-gray-200 rounded-t-md">
+                        <div><label for="selectAll"><input type="checkbox" class="mr-2" name="select_all" id="select_all">Select All</label></div>
+                        <div class="text-right mr-5">Total: <b>RM9999.00</b></div>
+                    </div>
                     @foreach ($records as $record)
-                        <tr>
-                            <td><input type="checkbox" name="row" id="row"></td>
-                            <td>{{ $record->category_id->category_name }}</td>
-                            <td>{{ $record->date, $record->time }}</td>
-                            <td>{{ $record->record_description }}</td>
-                            <td>{{ $record->account_id->user()->name }}</td>
-                            <td>{{ $record->amount }}</td>
-                        </tr>
+                        <div class="grid grid-cols-6 px-5 border border-black bg-gray-200 items-center" style="height: 45px">
+                            <div class="flex items-center"><input type="checkbox" name="row" id="row"></div> 
+                            <div>{{ $record->category->category_name }}</div>
+                            <div>{{ $record->date }} {{ $record->time }}</div>
+                            <div>{{ $record->record_description }}</div>
+                            <div>{{ $record->user->name }}</div>
+                            <div class="text-right">RM {{ $record->amount }}
+                                <i class="fa-solid fa-ellipsis-vertical ml-3"></i></div>
+                        </div>
                     @endforeach
-                </table>
+                </div>
             @else
                 <p class="m-3 flex justify-center" style="font-size: 20px">No records found.</p>
             @endif
