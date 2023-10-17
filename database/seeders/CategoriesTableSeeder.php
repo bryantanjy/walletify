e432fd14533c08de56e04cac027385d5ff6b793c
Bryan Tan <bryantan8858@gmail.com>
2023-10-17 23:14:24 +0800

    seeder

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -14,20 +14,20 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         $categories = [
-            ['category_name' => 'Food & Drink'],
-            ['category_name' => 'Housing'],
-            ['category_name' => 'Shopping'],
-            ['category_name' => 'Travel'],
-            ['category_name' => 'Transportation'],
-            ['category_name' => 'Utilities'],
-            ['category_name' => 'Entertainment'],
-            ['category_name' => 'Education'],
-            ['category_name' => 'Personal Care'],
-            ['category_name' => 'Gift & Donations'],
-            ['category_name' => 'Investments'],
-            ['category_name' => 'Health & Fitness'],
-            ['category_name' => 'Miscellaneous'],
-            ['category_name' => 'Income'],
+            ['category_id' => 'C01', 'category_name' => 'Food & Drink'],
+            ['category_id' => 'C02', 'category_name' => 'Housing'],
+            ['category_id' => 'C03', 'category_name' => 'Shopping'],
+            ['category_id' => 'C04', 'category_name' => 'Travel'],
+            ['category_id' => 'C05', 'category_name' => 'Transportation'],
+            ['category_id' => 'C06', 'category_name' => 'Utilities'],
+            ['category_id' => 'C07', 'category_name' => 'Entertainment'],
+            ['category_id' => 'C08', 'category_name' => 'Education'],
+            ['category_id' => 'C09', 'category_name' => 'Personal Care'],
+            ['category_id' => 'C10', 'category_name' => 'Gift & Donations'],
+            ['category_id' => 'C11', 'category_name' => 'Investments'],
+            ['category_id' => 'C12', 'category_name' => 'Health & Fitness'],
+            ['category_id' => 'C13', 'category_name' => 'Miscellaneous'],
+            ['category_id' => 'C14', 'category_name' => 'Income'],
         ];
 
         // Insert the categories into the 'categories' table
---
+++ database/seeders/RecordTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Record;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RecordTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Record::factory()->count(30)->create();
+    }
+}
