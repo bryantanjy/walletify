38333ca3866faa1083254b17e30d2871ded036e6
Bryan Tan <bryantan8858@gmail.com>
2023-11-11 17:14:43 +0800

    add font-awesome cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
 
         <!-- Styles -->
         @livewireStyles
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     </head>
     <body class="font-sans antialiased">
         <x-banner />
