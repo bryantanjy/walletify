0ffcb45416e83e9773c25963f0e1071821a2c7a6
Bryan Tan <bryantan8858@gmail.com>
2023-11-15 15:25:39 +0800

    add datatable render, update validation

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -6,7 +6,11 @@ use App\Models\Record;
 use App\Models\Account;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\Console\Input\Input;
 
 class RecordController extends Controller
 {
@@ -14,25 +18,25 @@ class RecordController extends Controller
     {
         if (auth()->check()) {
             $user = Auth::user();
-            $query = Record::where('user_id', $user->id);
+            $records = Record::where('user_id', $user->id)->get();
 
-            if ($request->has('sort')) {
-                $sortOption = $request->input('sort');
-                if ($sortOption == 'oldest') {
-                    $query->orderBy('date', 'asc')->orderBy('time', 'asc');
-                } else {
-                    $query->orderBy('date', 'desc')->orderBy('time', 'desc');
-                }
-            } else {
-                $query->orderBy('date', 'desc')->orderBy('time', 'desc');
-            }
+            // if ($request->has('sort')) {
+            //     $sortOption = $request->input('sort');
+            //     if ($sortOption == 'oldest') {
+            //         $query->orderBy('date', 'asc');
+            //     } else {
+            //         $query->orderBy('date', 'desc');
+            //     }
+            // } else {
+            //     $query->orderBy('date', 'desc');
+            // }
 
-            $records = $query->get();
+            // $records = $query->get();
             $totalExpesne = 0;
             $totalIncome = 0;
 
             foreach ($records as $record) {
-                if ($record->record_type === 'Expense') {
+                if ($record->type === 'Expense') {
                     $totalExpesne += $record->amount;
                 } else {
                     $totalIncome += $record->amount;
@@ -49,10 +53,43 @@ class RecordController extends Controller
         }
     }
 
+    // rendering datatable
+    public function recordList()
+    {
+        $user = Auth::user();
+        $records = Record::where('user_id', $user->id)->with('user', 'category')
+            ->with(['user' => function ($query) {
+                $query->select('id', 'name');
+            }, 'category' => function ($query) {
+                $query->select('id', 'name');
+            }])
+            ->select(['id','user_id', 'category_id', 'type', 'datetime', 'description', 'amount'])
+            ->get();
+
+
+        return DataTables::of($records)
+            ->addColumn('user', function (Record $record) {
+                return $record->user->name;
+            })
+            ->addColumn('category', function (Record $record) {
+                return $record->category->name;
+            })
+            ->addColumn('action', function ($record) {
+                // Update Button
+                $editButton = "<button class='editRecord' data-id='" . $record->id . "' data-bs-toggle='modal' data-bs-target='#editRecordModal'><i class='fa-solid fa-pen-to-square'></i></button>";
+
+                // Delete Button
+                $deleteButton = "<button class='deleteRecord' data-id='" . $record->id . "'><i class='fa-solid fa-trash'></i></button>";
+
+                return $editButton . " " . $deleteButton;
+            })
+            ->make(true);
+    }
+
     public function create()
     {
         $user = Auth::user();
-        $account = Account::where('user_id', $user->id)->get();
+        $accounts = Account::where('user_id', $user->id)->get();
         $categories = Category::all();
 
         return view('record.create', compact('accounts', 'categories'));
@@ -60,20 +97,33 @@ class RecordController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'account_id' => 'required',
-            'category_id' => 'required|string',
-            'record_type' => 'required|string',
-            'amount' => 'required|numeric',
-            'date' => 'required',
-            'time' => 'required',
-            'record_description' => 'nullable|string',
-            'group_id' => 'nullable',
-        ]);
-        $validatedData['user_id'] = auth()->id();
-        
-        $record = new Record($validatedData);
-        $record->save();
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'account_id' => 'required',
+                'category_id' => 'required',
+                'type' => 'required|string',
+                'amount' => 'required|numeric',
+                'datetime' => 'required',
+                'description' => 'nullable|string',
+                'group_id' => 'nullable',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        } else {
+            $record = new Record;
+            $record->account_id = $request->input('account_id');
+            $record->category_id = $request->input('category_id');
+            $record->user_id = auth()->id();
+            $record->type = $request->input('type');
+            $record->amount = $request->input('amount');
+            $record->datetime = $request->input('datetime');
+            $record->description = $request->input('description');
+            $record->group_id = $request->input('group_id');
+            $record->save();
+        }
         return redirect()->route('record.index')->with('success', 'Record added successfully');
     }
 
@@ -84,34 +134,51 @@ class RecordController extends Controller
 
     public function edit($recordId)
     {
-        $record = Record::findOrFail($recordId);
-        $categories = Category::all();
-        $accounts = Account::where('user_id', Auth::user()->id)->get();
-        
-        return view('record.edit', compact('record', 'accounts', 'categories'));
+        $record = Record::find($recordId);
+        if (!$record) {
+            // Handle the case where the record is not found, for example, redirect to an error page.
+            return response()->json(['errors' => 'Record not found']);
+        }
+        // $categories = Category::all();
+        // $accounts = Account::where('user_id', Auth::user()->id)->get();
+        return response()->json($record);
+        // return view('record.edit', compact('record', 'accounts', 'categories'));
     }
 
-
-    public function update(Request $request, Record $record)
+    public function update(Request $request, $recordId)
     {
-        $recordId = $request->input('record_id');
+        $record = Record::find($recordId);
 
-        $validatedData = $request->validate([
-            'account_id' => 'required',
-            'category_id' => 'required',
-            'record_type' => 'required|string',
-            'amount' => 'required|numeric',
-            'date' => 'required|date',
-            'time' => 'required',
-            'record_description' => 'nullable|string',
-            'group_id' => 'nullable',
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'account_id' => 'required',
+                'category_id' => 'required',
+                'type' => 'required|string',
+                'amount' => 'required|numeric',
+                'datetime' => 'required',
+                'description' => 'nullable|string',
+                'group_id' => 'nullable',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect()->route('record.edit')
+                ->withErrors($validator);
+        }
+
+        $record->update([
+            'user_id' => $request->input('user_id'),
+            'account_id' => $request->input('account_id'),
+            'category_id' => $request->input('category_id'),
+            'type' => $request->input('type'),
+            'amount' => $request->input('amount'),
+            'datetime' => $request->input('datetime'),
+            'description' => $request->input('description'),
+            'group_id' => $request->input('group_id'),
         ]);
-        $validatedData['user_id'] = auth()->id();
 
-        $record = Record::findOrFail($recordId);
-        $record->update($validatedData);
-
-        return redirect()->route('record.index');
+        return redirect()->route('record.index')->with('success', 'Record updated successfully');
     }
 
     public function delete(Record $record)
