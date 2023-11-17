b295d3e729c4b00c0c33cef403f66d83a36c9698
Bryan Tan <bryantan8858@gmail.com>
2023-11-17 23:26:32 +0800

    update header

--- resources/views/budget/createUserTemplate.blade.php
+++ resources/views/budget/createUserTemplate.blade.php
@@ -4,7 +4,7 @@
         {{-- Modal Content --}}
         <div class="modal-content relative p-4 rounded-lg sm:p-5" style="background-color: #E1F1FA">
             <div class="modal-header flex justify-between items-center">
-                <h2 style="font-size:20px;"><b>Create new template</b></h2>
+                <h2 style="font-size:20px;"><b>Create New Budget</b></h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                     <i class="fa-solid fa-xmark"></i>
                 </button>
