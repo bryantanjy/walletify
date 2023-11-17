772d22e0e6432bf4d03348224ec0287ec9e5475e
Bryan Tan <bryantan8858@gmail.com>
2023-11-17 15:54:58 +0800

    hide javascript-createUserTemplate by commenting

--- public/js/modal.js
+++ public/js/modal.js
@@ -68,63 +68,55 @@ function showDeleteModal(accountId) {
 // }
 
 // Budget Section
-
-
 // js for create budget template
-document.addEventListener("DOMContentLoaded", function () {
-    // user template js
-    const generateButton = document.getElementById("generateFields");
-    const partContainer = document.getElementById("partContainer");
-    const buttonsDiv = document.getElementById('buttons');
+// document.addEventListener("DOMContentLoaded", function () {
+//     // user template js
+//     const generateButton = document.getElementById("generateFields");
+//     const partContainer = document.getElementById("partContainer");
+//     const buttonsDiv = document.getElementById('buttons');
 
 
-    generateButton.addEventListener("click", function () {
-        const numberOfParts = parseInt(document.getElementById('input').value);
+//     generateButton.addEventListener("click", function () {
+//         const numberOfParts = parseInt(document.getElementById('input').value);
 
-        if (numberOfParts >= 1 && numberOfParts <= 5) {
-            partContainer.innerHTML = "";
+//         if (numberOfParts >= 1 && numberOfParts <= 5) {
+//             partContainer.innerHTML = "";
 
-            for (let i = 1; i <= numberOfParts; i++) {
-                const partDiv = document.createElement("div");
-                const partIndex = i;
+//             for (let i = 1; i <= numberOfParts; i++) {
+//                 const partDiv = document.createElement("div");
+//                 const partIndex = i;
 
-                partDiv.innerHTML = `
-                    <h4 style="font-size:20px; margin-top:25px"><b>Part ${partIndex}</b></h4>
-                    <div class="flex items-center">
-                        <label for="partName${partIndex}" class="w-32 pr-2 mt-4">Name</label>
-                        <input class="rounded-md" type="text" name="part_name[${partIndex}]" id="part_name${partIndex}" placeholder="Name"
-                            style="height: 30px; margin:15px 0px 0px 20px;width:175px;" required>
-                    </div>
-                    <div class="flex items-center">
-                        <label for="partAmount${partIndex}" class="w-32 pr-2 mt-4">Amount</label>
-                        <input class="rounded-md" type="number" step="0.01" name="allocation_amount[${partIndex}]" id="allocation_amount${partIndex}" placeholder="0.00"
-                            style="height: 30px; margin:15px 0px 0px 20px;text-align:right;width:175px;" required>
-                    </div>
-                    <div class="flex">
-                        <label for="partCategory${partIndex}" class="w-32 pr-2 mt-4">Category</label>
-                        <select class="rounded-md categorySelect"  name="category_id[${partIndex}][]" id="category_id${partIndex}" multiple
-                            required>
-                            ${categories.map(category => `<option value="${category.id}">${category.name}</option>`).join('')}
-                        </select>
-                    </div>
-                `;
+//                 partDiv.innerHTML = `
+//                     <h4 style="font-size:20px; margin-top:25px"><b>Part ${partIndex}</b></h4>
+//                     <div class="flex items-center">
+//                         <label for="partName${partIndex}" class="w-32 pr-2 mt-4">Name</label>
+//                         <input class="rounded-md border-0" type="text" name="part_name[${partIndex}]" id="part_name${partIndex}" placeholder="Name"
+//                             style="height: 30px; margin:15px 0px 0px 20px;width:175px;" required>
+//                     </div>
+//                     <div class="flex items-center">
+//                         <label for="partAmount${partIndex}" class="w-32 pr-2 mt-4">Amount</label>
+//                         <input class="rounded-md border-0" type="number" step="0.01" name="allocation_amount[${partIndex}]" id="allocation_amount${partIndex}" placeholder="0.00"
+//                             style="height: 30px; margin:15px 0px 0px 20px;text-align:right;width:175px;" required>
+//                     </div>
+//                     <div class="flex">
+//                         <label for="partCategory${partIndex}" class="w-32 pr-2 mt-4">Category</label>
+//                         <select class="rounded-md categorySelect border-0" name="category_id[${partIndex}][]" id="category_id${partIndex}" multiple
+//                             required>
+//                             ${categories.map(category => `<option value="${category.id}">${category.name}</option>`).join('')}
+//                         </select>
+//                     </div>
+//                 `;
 
-                partContainer.appendChild(partDiv);
+//                 partContainer.appendChild(partDiv);
 
-                $('#category_id' + partIndex).filterMultiSelect({
-                    placeholderText: "Please select",
-                });
-            }
-            buttonsDiv.style.display = "block";
-        }
-    });
-
-    // Default template js
-
-});
-
-// js for edit budget
-
+//                 $('#category_id' + partIndex).filterMultiSelect({
+//                     placeholderText: "Please select",
+//                 });
+//             }
+//             buttonsDiv.style.display = "block";
+//         }
+//     });
+// });
 
 // modal delete for budget
 function budgetDeleteModal(budgetId) {
