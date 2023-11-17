c5b4de202be447ff6b6cb2288009b396ec7285b3
Bryan Tan <bryantan8858@gmail.com>
2023-11-17 20:46:42 +0800

    change styling and javascript for create part

--- resources/views/budget/createUserTemplate.blade.php
+++ resources/views/budget/createUserTemplate.blade.php
@@ -1,6 +1,6 @@
 <div id="createBudgetTemplateModal" class="modal fade" tabindex="-1" role="dialog"
     aria-labelledby="budgetTemplateSelectionModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg relative p-4 w-full  h-full md:h-auto" role="document">
+    <div class="modal-dialog modal-lg relative p-4 w-full h-full md:h-auto" role="document">
         {{-- Modal Content --}}
         <div class="modal-content relative p-4 rounded-lg sm:p-5" style="background-color: #E1F1FA">
             <div class="modal-header flex justify-between items-center">
@@ -9,19 +9,12 @@
                     <i class="fa-solid fa-xmark"></i>
                 </button>
             </div>
-            <div class="modal-body flex flex-col">
-                {{-- <div>
-                    <label for="part_input">How many part do you want?</label>
-                    <input class="rounded-md mx-3 border-0" type="number" name="input" id="input" min="1"
-                        max="5" style="height: 30px;width: 100px" placeholder="e.g. 1" required>
-                    <button type="submit" id="generateFields" class="bg-blue-500 rounded"
-                        style="width: 80px; height: 30px;">Enter</button>
-                </div> --}}
+            <div class="modal-body ">
                 <form method="POST" action="{{ route('budget.storeUserTemplate') }}">
                     @csrf
                     <div id="dynamicPartFields"></div>
                     <button type="button" class="bg-white border rounded mt-3" id="plusButton"
-                        style="width: 35px; height: 35px; font-size: 22px">+</button>
+                        style="width: 35px; height: 35px; font-size: 22px; display:none">+</button>
                     <input type="hidden" name="type" id="type" value="User Template">
                     <div class="float-right mt-4" id="buttons">
                         <button type="submit" class="bg-blue-400 rounded hover:bg-blue-300"
@@ -40,27 +33,23 @@
 
     const container = document.getElementById('dynamicPartFields');
     const plusButton = document.getElementById('plusButton');
-
+    plusButton.style.display = 'block';
     createPart(1);
 
     plusButton.addEventListener('click', function() {
-        if (container.childElementCount < 5) {
+        if (container.childElementCount <= 4) {
             const nextPartIndex = container.childElementCount + 1;
             createPart(nextPartIndex);
-
-            if (container.childElementCount === 5) {
-                plusButton.disabled = true;
-                plusButton.style.display = 'none';
-            }
-        }
+        } 
     });
 
     function createPart(partIndex) {
         const partField = document.createElement('div');
         partField.className = 'part-field';
+        partField.id = `partField${partIndex}`;
 
         partField.innerHTML = `
-                <h4 style="font-size:20px; margin-top:25px"><b>Part ${partIndex}</b></h4>
+                <h4 style="font-size:20px; margin-top:25px">Part ${partIndex}</h4>
                 <div class="flex items-center">
                     <label for="partName${partIndex}" class="w-32 pr-2 mt-4">Name</label>
                     <input class="rounded-md border-0" type="text" name="part_name[${partIndex}]" id="part_name${partIndex}" placeholder="Name"
@@ -68,22 +57,57 @@
                 </div>
                 <div class="flex items-center">
                     <label for="partAmount${partIndex}" class="w-32 pr-2 mt-4">Amount</label>
-                    <input class="rounded-md border-0" type="number" step="0.01" name="allocation_amount[${partIndex}]" id="allocation_amount${partIndex}" placeholder="0.00"
+                    <input class="rounded-md border-0" type="number" step="0.01" name="amount[${partIndex}]" id="amount${partIndex}" placeholder="0.00"
                         style="height: 30px; margin:15px 0px 0px 20px;text-align:right;width:175px;" required>
                 </div>
                 <div class="flex">
                     <label for="partCategory${partIndex}" class="w-32 pr-2 mt-4">Category</label>
-                    <select class="rounded-md categorySelect border-0" name="category_id[${partIndex}][]" id="category_id${partIndex}" multiple
+                    <select class="rounded-md categorySelect border-0" name="categoryId[${partIndex}][]" id="categoryId${partIndex}" multiple
                         required>
                         ${categories.map(category => `<option value="${category.id}">${category.name}</option>`).join('')}
                     </select>
                 </div>
+                <button type="button" class="bg-white border rounded mt-2 float-right" id="removeButton${partIndex}"
+                    style="width: 35px; height: 35px; font-size: 22px;">-</button>
             `;
 
         container.appendChild(partField);
 
-        $('#category_id' + partIndex).filterMultiSelect({
+        $('#categoryId' + partIndex).filterMultiSelect({
             placeholderText: "Please select",
+        });
+
+        const removeButton = document.getElementById(`removeButton${partIndex}`);
+        if (container.childElementCount === 1) {
+            removeButton.style.display = "none";
+        } else {
+            removeButton.addEventListener('click', function() {
+                container.removeChild(partField);
+                updatePartNumbers();
+            });
+        }
+    }
+
+    function updatePartNumbers() {
+        const partFields = container.querySelectorAll('.part-field');
+        partFields.forEach((partField, index) => {
+            const partIndex = index + 1;
+            const partHeader = partField.querySelector('h4');
+            const partLabelText = partHeader.innerText;
+            partHeader.innerText = `Part ${partIndex}`;
+            partField.id = `partField${partIndex}`;
+            // Update the input names and IDs
+            partField.querySelectorAll('[id^=part_name]').forEach(input => input.name =
+                `part_name[${partIndex}]`);
+            partField.querySelectorAll('[id^=amount]').forEach(input => input.name =
+                `amount[${partIndex}]`);
+            partField.querySelectorAll('[id^=categoryId]').forEach(select => {
+                select.name = `categoryId[${partIndex}][]`;
+                select.id = `categoryId${partIndex}`;
+            });
+            // Update the button ID
+            const removeButton = partField.querySelector('[id^=removeButton]');
+            removeButton.id = `removeButton${partIndex}`;
         });
     }
 </script>
@@ -103,7 +127,11 @@
         right: 0;
     }
 
-    .filter-multi-select>.viewbar> span.placeholder {
+    .filter-multi-select>.viewbar {
+        border: 0;
+    }
+
+    .filter-multi-select>.viewbar>span.placeholder {
         background-color: transparent;
         cursor: pointer;
     }
