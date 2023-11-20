56ce2b90c45339f9436899f32bcb9ee337e3b347
Bryan Tan <bryantan8858@gmail.com>
2023-11-21 00:45:03 +0800

    add group member model

---
+++ app/Models/GroupMember.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class GroupMember extends Model
+{
+    use HasFactory;
+    use HasRoles;
+
+    protected $fillable = ['user_id', 'group_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(ExpenseSharingGroup::class);
+    }
+}
