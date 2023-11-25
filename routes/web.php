8f519ede20a9f625d31b087c181e162b73be291a
Bryan Tan <bryantan8858@gmail.com>
2023-11-25 17:39:29 +0800

    add comment and expense sharing routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AccountController;
 use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\RecordController;
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\StatisticController;
+use App\Http\Controllers\ExpenseSharingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,39 +32,61 @@ Route::middleware([
     })->name('dashboard');
 });
 
+
+// route for account module
 Route::group(['prefix' => 'account'], function () {
-    Route::get('/create', [AccountController::class, 'create'])->name('account.create');
     Route::get('/', [AccountController::class, 'index'])->name('account.index');
+    Route::get('/create', [AccountController::class, 'create'])->name('account.create');
     Route::post('/store', [AccountController::class, 'store'])->name('account.store');
     Route::get('/edit/{account}', [AccountController::class, 'edit'])->name('account.edit');
     Route::put('/update/{account}', [AccountController::class, 'update'])->name('account.update');
     Route::delete('/delete/{account}', [AccountController::class, 'delete'])->name('account.delete');
 });
 
+// route for record module
 Route::group(['prefix' => 'record'], function () {
-    Route::get('/create', [RecordController::class, 'create'])->name('record.create');
     Route::get('/', [RecordController::class, 'index'])->name('record.index');
+    Route::get('/create', [RecordController::class, 'create'])->name('record.create');
     Route::post('/store', [RecordController::class, 'store'])->name('record.store');
     Route::get('/edit/{record}', [RecordController::class, 'edit'])->name('record.edit');
     Route::put('/update/{record}', [RecordController::class, 'update'])->name('record.update');
     Route::delete('/delete/{record}', [RecordController::class, 'delete'])->name('record.delete');
 });
 
+// route for budget module
 Route::group(['prefix' => 'budget'], function () {
-    Route::get('/createUserTemplate', [BudgetController::class, 'createUserTemplate'])->name('budget.createUserTemplate');
-    Route::get('/createDefaultTemplate', [BudgetController::class, 'createDefaultTemplate'])->name('budget.createDefaultTemplate');
     Route::get('/', [BudgetController::class, 'index'])->name('budget.index');
-    Route::post('/storeDefaultTemplate', [BudgetController::class, 'storeDefaultTemplate'])->name('budget.storeDefaultTemplate');
+    Route::get('/createUserTemplate', [BudgetController::class, 'createUserTemplate'])->name('budget.createUserTemplate');
     Route::post('/storeUserTemplate', [BudgetController::class, 'storeUserTemplate'])->name('budget.storeUserTemplate');
-    Route::get('/editDefaultTemplate/{budget}', [BudgetController::class, 'editDefaultTemplate'])->name('budget.editDefaultTemplate');
     Route::get('/editUserTemplate/{budget}', [BudgetController::class, 'editUserTemplate'])->name('budget.editUserTemplate');
-    Route::put('/updateDefaultTemplate/{budget}', [BudgetController::class, 'updateDefaultTemplate'])->name('budget.updateDefaultTemplate');
     Route::put('/updateUserTemplate/{budget}', [BudgetController::class, 'updateUserTemplate'])->name('budget.updateUserTemplate');
+    Route::get('/createDefaultTemplate', [BudgetController::class, 'createDefaultTemplate'])->name('budget.createDefaultTemplate');
+    Route::post('/storeDefaultTemplate', [BudgetController::class, 'storeDefaultTemplate'])->name('budget.storeDefaultTemplate');
+    Route::get('/editDefaultTemplate/{budget}', [BudgetController::class, 'editDefaultTemplate'])->name('budget.editDefaultTemplate');
+    Route::put('/updateDefaultTemplate/{budget}', [BudgetController::class, 'updateDefaultTemplate'])->name('budget.updateDefaultTemplate');
     Route::delete('/delete/{budget}', [BudgetController::class, 'delete'])->name('budget.delete');
 });
 
+// route for statistic module
 Route::group(['prefix' => 'statistic'], function () {
     Route::get('/', [StatisticController::class, 'index'])->name('statistic.index');
     Route::get('/expense', [StatisticController::class, 'expense'])->name('statistic.expense');
     Route::get('/income', [StatisticController::class, 'income'])->name('statistic.income');
+});
+
+// route for expense sharing & group module
+Route::group(['prefix' => 'expense-sharing', 'as' => 'expense-sharing.'], function () {
+    Route::get('/', [ExpenseSharingController::class, 'index'])->name('index');
+    Route::get('/create', [ExpenseSharingController::class, 'create'])->name('create');
+    Route::post('/store', [ExpenseSharingController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [ExpenseSharingController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [ExpenseSharingController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [ExpenseSharingController::class, 'delete'])->name('delete');
+
+
+    Route::group(['prefix' => 'group', 'as' => 'group.'], function () {
+        Route::get('/', [GroupController::class, 'index'])->name('index');
+        Route::get('/add-participant', [GroupController::class, 'addParticipant'])->name('addParticipant');
+        Route::get('/edit-participant/{id}', [GroupController::class, 'editParticipant'])->name('editParticipant');
+    });
 });
