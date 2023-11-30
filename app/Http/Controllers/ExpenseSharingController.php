d6175a642d603d2dd3e7582be27a03c1580edeca
Bryan Tan <bryantan8858@gmail.com>
2023-11-30 11:32:38 +0800

    update method name and add index & store logic

--- app/Http/Controllers/ExpenseSharingController.php
+++ app/Http/Controllers/ExpenseSharingController.php
@@ -2,32 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExpenseSharingGroup;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ExpenseSharingController extends Controller
 {
-    public function createGroup()
-    {
-        return view('expense-sharing.create-group');
-    }
-
-    public function editGroup()
-    {
-        return view('expense-sharing.edit-group');
-    }
-
     public function index()
-    {
-        return view('expense-sharing.index');
+    {   
+        if(Auth::check()) {
+            $user = auth()->user();
+            $groups = ExpenseSharingGroup::where('user_id', $user->id)->get();
+
+            return view('expense-sharing.index', compact('groups'));
+        } else {
+            redirect('/login');
+        }
+         
+        
     }
 
-    public function addParticipant()
+    public function create()
     {
-        return view('expense-sharing.add-participant');
+        return view('expense-sharing.create');
     }
 
-    public function editUser()
+    public function store(Request $request)
     {
-        return view('expense-sharing.edit-user');
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string|max:255',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
+
+        $expenseSharing = new ExpenseSharingGroup;
+        $expenseSharing->name = $request->input('name');
+        $expenseSharing->description = $request->input('description');
+        $expenseSharing->save();
+
+        return view('expense-sharing.index')->with('success', 'Your Expense Sharing Group created successfully');
+    }
+
+    public function edit()
+    {
+        return view('expense-sharing.edit');
+    }
+
+    public function update()
+    {
+        return view('expense-sharing.update');
+    }
+
+    public function delete()
+    {
+        return view('expense-sharing.delete');
     }
 }
