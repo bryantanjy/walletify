382c308a8ea22dfd0aad44111aa7e63d2da9c732
Bryan Tan <bryantan8858@gmail.com>
2023-12-28 21:41:10 +0800

    add dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Record;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Models\ExpenseSharingGroup;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $userId = auth()->id();
+        $activeGroupId = session('active_group_id');
+        $groups = ExpenseSharingGroup::where('user_id', $userId)->get();
+
+        // Determine the current session type
+        $currentSession = config('app.user_session_type', 'personal');
+
+        if ($activeGroupId) {
+            $group = ExpenseSharingGroup::findOrFail($activeGroupId);
+
+            $monthExpenses = Record::where('expense_sharing_group_id', $group)
+                ->where('type', 'Expense')
+                ->whereMonth('datetime', now()->month)
+                ->whereYear('datetime', now()->year)
+                ->groupBy('category_id')
+                ->selectRaw('category_id, sum(amount) as total_amount')
+                ->with('category')
+                ->get();
+
+            $recentRecords = Record::where('expense_sharing_group_id', $group)
+                ->orderBy('datetime', 'desc')
+                ->take(10)
+                ->get();
+
+            $records = Record::where('expense_sharing_group_id', $group)
+                ->whereMonth('datetime', now()->month)
+                ->whereYear('datetime', now()->year)
+                ->get();
+        } else {
+            $monthExpenses = Record::where('user_id', $userId)
+                ->where('expense_sharing_group_id', null)
+                ->where('type', 'Expense')
+                ->whereMonth('datetime', now()->month)
+                ->whereYear('datetime', now()->year)
+                ->groupBy('category_id')->selectRaw('category_id, sum(amount) as total_amount')
+                ->with('category')
+                ->get();
+
+            $recentRecords = Record::where('user_id', $userId)
+                ->where('expense_sharing_group_id', null)
+                ->orderBy('datetime', 'desc')
+                ->take(10)
+                ->get();
+
+            $records = Record::where('user_id', $userId)
+                ->where('expense_sharing_group_id', null)
+                ->whereMonth('datetime', now()->month)
+                ->whereYear('datetime', now()->year)
+                ->get();
+        }
+
+        return view('dashboard', compact('groups', 'monthExpenses', 'recentRecords', 'records', 'currentSession'));
+    }
+}
