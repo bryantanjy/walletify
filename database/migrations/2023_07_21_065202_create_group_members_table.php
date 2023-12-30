3f341af1d3819eed93e2f36b1c48dd328d1aed62
Bryan Tan <bryantan8858@gmail.com>
2023-12-30 19:04:49 +0800

    remove id, change role_id to role, add unique to table

--- database/migrations/2023_07_21_065202_create_group_members_table.php
+++ database/migrations/2023_07_21_065202_create_group_members_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('group_members', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->unsignedBigInteger('expense_sharing_group_id');
             $table->foreign('expense_sharing_group_id')->references('id')->on('expense_sharing_groups')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('role_id')->constrained('roles');
+            $table->string('role');
             $table->timestamps();
 
-            $table->unique(['user_id', 'expense_sharing_group_id']); // Ensure uniqueness of user-group pairs
+            // Add a unique constraint if you want to ensure uniqueness of user-group-role combinations
+            $table->unique(['user_id', 'expense_sharing_group_id', 'role']);
         });
     }
 
