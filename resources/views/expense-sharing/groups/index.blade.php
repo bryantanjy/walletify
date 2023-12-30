2c412b3117a8dab71f842afd2279f4ffbcbb4d62
Bryan Tan <bryantan8858@gmail.com>
2023-12-30 16:17:56 +0800

    remove duplication

--- resources/views/expense-sharing/groups/index.blade.php
+++ resources/views/expense-sharing/groups/index.blade.php
@@ -47,12 +47,12 @@
                     <div class="rounded-md bg-white mb-3">
                         <div class="grid grid-cols-4 items-center rounded-md px-5 hover:bg-gray-100"
                             style="height: 50px">
-                            <div class="col-start-1 col-end-2" id="group_name">
+                            <div class="col-start-1 col-end-2" id="username">
                                 {{ $member->user->name }}
                             </div>
                             <div class="col-start-2 col-end-3" style="font-size: 14px; color:gray"
-                                id="group_description">
-                                {{ $member->roles }}
+                                id="role">
+                                {{ $member->roles->getRoleNames() }}
                             </div>
                             <div class="col-start-5 col-end-5">
                                 <button class="editGroupBtn mr-4" value="{{ $member->id }}" data-bs-toggle="modal"
@@ -86,7 +86,7 @@
 
             <div class="modal-body">
                 <!-- Group invite form -->
-                <form action="" method="POST">
+                <form action="{{ route('expense-sharing.groups.send-invitation', ['groupId' => $group->id]) }}" method="POST">
                     @csrf
                     <div class="mb-3 text-left">
                         <label for="email" class="mb-2">Enter Participant's Email</label><br>
@@ -96,39 +96,6 @@
                     <div class="flex mt-6 justify-center text-white">
                         <button type="submit"
                             style="background: #4deb67; width:175px; height:30px; border:0px solid; border-radius: 5px; font-weight:bold">Send
-                            Invitation</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div id="inviteUserModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="inviteUserModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <!--Modal Content-->
-        <div class="modal-content relative p-4 text-center rounded-lg sm:p-5" style="background-color: #E1F1FA">
-            <div class="modal-header flex items-center justify-between">
-                <h2 class="modal-title" style="font-weight: bold; font-size:20px">Send Invitation</h2>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    <i class="fa-solid fa-xmark"></i>
-                </button>
-            </div>
-
-            <div class="modal-body">
-                <!-- Group invite form -->
-                <form action="{{ route('expense-sharing.groups.send-invitation', ['groupId' => $group->id]) }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="accountName">Enter Participant's Email</label>
-                        <input type="email" name="email" class="rounded-md border-0"
-                            style="height: 30px; padding:0px 10px; margin:15px 0px 0px 11px; width:50%"
-                            placeholder="user@email.com" required>
-                    </div>
-                    <div class="flex mt-6 justify-center text-white">
-                        <button type="submit"
-                            style="background: #4deb67; width:100px; height:26px; border:0px solid; border-radius: 5px">Send
                             Invitation</button>
                     </div>
                 </form>
