984f9c712e259ad3315d392804d72a7643aa89b4
bryan <bryantan8858@gmail.com>
2024-03-25 10:05:37 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(AccountsTableSeeder::class);
-        $this->call(RecordsTableSeeder::class);
+        // $this->call(RecordsTableSeeder::class);
         $this->call(PermissionSeeder::class);
     }
 }
